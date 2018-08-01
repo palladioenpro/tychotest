1e40681bddc325622e8eb2cbce0c326e0e3ac5e2
domasm@web.de <domasm@web.de>
2018-08-01 12:44:36 +0200

    branch test

--- org.palladiosimulator.measurementsui/bundles/HelloWorldMenu/src/helloworldmenu/handlers/SampleHandler.java
+++ org.palladiosimulator.measurementsui/bundles/HelloWorldMenu/src/helloworldmenu/handlers/SampleHandler.java
@@ -314,7 +314,7 @@ public class SampleHandler extends AbstractHandler {
 		return null;
 	}
 	public static int test(String test) {
-		if (test.equals("test3")) {
+		if (test.equals("test4")) {
 			return 1;
 		}
 		return 0;
